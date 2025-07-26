830c53a6bd7c469ac2ac7d6a47c1c869823e6ffc
Justin <33402995+logJustin@users.noreply.github.com>
2025-07-26 12:38:56 -0500

    typo in duration

--- scripts/message/analysis/utils.py
+++ scripts/message/analysis/utils.py
@@ -108,7 +108,7 @@ def duration_listened_today(song_list):
         
         human_readable_duration = ''
         
-        if hours == 0:
+        if hours > 0:
             human_readable_duration = f'{hours} hours, {minutes} minutes, and {seconds} seconds'
         if minutes > 0: 
             human_readable_duration = f'{minutes} minutes and {seconds} seconds'
